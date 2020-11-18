c171533800f38242841bd2e1f16b1790d0b09dbc
Egor Boldin <boldr@mail.ru>
2020-11-18 23:02:05 +0300

    initial commit

---
+++ src/main/java/ru/boldr/memebot/MemebotApplication.java
@@ -0,0 +1,13 @@
+package ru.boldr.memebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemebotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/boldr/memebot/MemebotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.boldr.memebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
