3b7cfe6b75191386db49b6ad52e5a9f16dea2cba
boldrqwe <70112965+boldrqwe@users.noreply.github.com>
2022-06-04 12:31:21 +0300

    добавил отправку видео

--- src/main/java/ru/boldr/memebot/executor/HarkachExecutor.java
+++ src/main/java/ru/boldr/memebot/executor/HarkachExecutor.java
@@ -1,20 +1,18 @@
 package ru.boldr.memebot.executor;
 
-import java.awt.Image;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
 import java.util.Stack;
-
-import javax.imageio.ImageIO;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.methods.send.SendVideo;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
@@ -54,28 +52,46 @@ public class HarkachExecutor {
                             InlineKeyboardButton.builder()
                                     .text("перейти в тред")
                                     .url(getUrl(threadComment.picture()))
-                                    .build(),
-                            InlineKeyboardButton.builder()
-                                    .text("скачать все фото из треда")
-                                    .callbackData("зов обратно")
                                     .build()
-
                     ))
             );
 
-            URL url = new URL(threadComment.picture());
+            String picture = threadComment.picture();
+            URL url = new URL(picture);
+
+            String[] split = picture.split("\\.");
+
+            var extension = split[split.length - 1];
+
             InputStream inputStream = url.openStream();
 
-            InputFile inputFile = new InputFile(inputStream, "photo");
 
-            SendPhoto sendPhoto = SendPhoto.builder()
-                    .chatId(chat.getChatId())
-                    .photo(inputFile)
-                    .caption(threadComment.comment())
-                    .replyMarkup(inlineKeyboard)
-                    .build();
 
-            telegramBot.executeAsync(sendPhoto);
+            InputFile inputFile = new InputFile(inputStream, "file");
+
+            String chatId = chat.getChatId();
+            String comment = threadComment.comment();
+
+            switch (extension) {
+                case ("jpg"), ("png") ->
+                        telegramBot.executeAsync(SendPhoto.builder()
+                        .chatId(chatId)
+                        .photo(inputFile)
+                        .caption(comment)
+                        .replyMarkup(inlineKeyboard)
+                        .build());
+
+                case ("mp4"), ("webm") ->  telegramBot.executeAsync(SendVideo.builder()
+                        .chatId(chatId)
+                        .caption(comment)
+                        .video(inputFile)
+                        .replyMarkup(inlineKeyboard)
+                        .build());
+
+                default -> throw new IllegalStateException("Unexpected value: " + extension);
+            }
+
+
 
         }
     }
